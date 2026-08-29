93a05142b28b6fd501762a566511f745db3c62d0
2454304hzy <27249303@qq.com>
2026-08-29 21:24:03 +0800

    Update syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -105,6 +105,9 @@ extern uint64 sys_uptime(void);
 extern uint64 sys_interpose(void);
 extern uint64 sys_sigalarm(void);
 extern uint64 sys_sigreturn(void);
+extern uint64 sys_symlink(void);
+extern uint64 sys_mmap(void);
+extern uint64 sys_munmap(void);
 
 static uint64 (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -131,6 +134,9 @@ static uint64 (*syscalls[])(void) = {
 [SYS_interpose] sys_interpose,
 [SYS_sigalarm] sys_sigalarm,
 [SYS_sigreturn] sys_sigreturn,
+[SYS_symlink] sys_symlink,
+[SYS_mmap]    sys_mmap,
+[SYS_munmap]  sys_munmap,
 };
 
 void
